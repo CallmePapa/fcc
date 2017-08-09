668f0b31a1e3ade596663f5bab458325e32fc7c7
CallmePapa <1062927638@qq.com>
2017-08-09 15:38:07 +0800

    Arguments Optional

---
+++ sorted union/arguments.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+function add() {
+    if (arguments.length == 1 && typeof arguments[0] == 'number') {
+        var num = arguments[0];
+        retuan
+        function add(a) {
+            if (typeof a == 'number')
+                return a + num;
+        };
+    }
+    else if (typeof  arguments[0] == 'number' && typeof arguments[1] == 'number') {
+        return arguments[0] + arguments[1];
+    }
+    else {
+        return undefined;
+    }
+
+}
+    add(2,3);
+
