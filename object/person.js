59a291d868d45f76b6878f4b7cab44893a58162b
CallmePapa <1062927638@qq.com>
2017-08-12 09:23:20 +0800

    high

--- object/person.js
+++ object/person.js
@@ -1,3 +1,34 @@
 /**
  * Created by weiqiujuan on 17-8-11.
  */
+var Person = function(firstAndLast) {
+    var name=firstAndLast.split(" "),
+        firstName=name[0],
+        lastName=name[1];
+    this.getFirstName=function(){
+        return firstName;
+    };
+    this.getLastName=function(){
+        return lastName;
+    };
+    this.getFullName=function(){
+        name[0]=firstName;
+        name[1]=lastName;
+        return name.join(' ');
+    };
+    this.setFirstName=function(first){
+        return firstName=first;
+    };
+    this.setLastName=function(last){
+        return lastName=last;
+    };
+    this.setFullName=function(firstAndLast){
+        name=firstAndLast.split(" ");
+        firstName=name[0];
+        lastName=name[1];
+    };
+
+};
+
+var bob = new Person('Bob Ross');
+bob.getFullName();
